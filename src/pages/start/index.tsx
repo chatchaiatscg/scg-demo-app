5468aa99f18329c566e11cf608b9fcd763dac8e6
Supavit Triampitak <supavitt@scg.com>
2022-04-04 17:36:35 +0700

    match UI with with Figma

--- src/pages/start/index.tsx
+++ src/pages/start/index.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import start from 'assets/images/start.png'
+import ReactPlayer from 'react-player'
+import {SimulateButton, ControllerButton} from "../home/component/ControllerButton";
+import {Container} from '@mui/material';
 
 const Start: React.FC = (): React.ReactElement => {
 
     return (
         <>
             <Link to="/">
-                <img
-                    alt="start"
-                    src={start}
-                    style={{objectFit: 'fill', width: '100%', height: '100vh'}}
-                />
+                <Container
+                    style={{
+                        position: 'absolute',
+                        left: '50%',
+                        top: '50%',
+                        transform: 'translate(-50%, -50%)'
+                    }}>
+                    <ReactPlayer
+                            url='video/AAF_AAQ_MERGED.mp4'
+                            playing
+                            width='100%'
+                            height='100%'
+                            loop
+                    />
+                    <SimulateButton></SimulateButton>
+                </Container>
             </Link>
         </>
     )
